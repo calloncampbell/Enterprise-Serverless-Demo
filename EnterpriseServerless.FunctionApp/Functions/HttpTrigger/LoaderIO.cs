4edae814398d196e4720fdaa53edc7ea681e3147
Callon Campbell <calloncampbell@msn.com>
2020-08-24 16:13:39 -0400

    Added document for Cosmos query optimization. Added Azure App Configuration as an example and use case of the service.

--- EnterpriseServerless.FunctionApp/Functions/HttpTrigger/LoaderIO.cs
+++ EnterpriseServerless.FunctionApp/Functions/HttpTrigger/LoaderIO.cs
@@ -8,26 +8,34 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
 namespace EnterpriseServerless.FunctionApp
 {
-    public static class LoaderIO
+    public class LoaderIO
     {
+        private static IConfigurationRoot _configuration { set; get; }
+        private readonly IConfigurationRefresher _configurationRefresher;
+
+        public LoaderIO(
+            IConfigurationRoot configuration,
+            IConfigurationRefresher configurationRefresher)
+        {
+            _configuration = configuration;
+            _configurationRefresher = configurationRefresher;
+        }
+
         [FunctionName(nameof(LoaderIO))]
-        public static IActionResult Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ExecutionContext context,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var config = new ConfigurationBuilder()
-                .SetBasePath(context.FunctionAppDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
+            await _configurationRefresher.TryRefreshAsync();
 
-            string loaderIOVerificationToken = config["LoaderIO_VerificationToken"];
+            string loaderIOVerificationToken = _configuration["EnterpriseServerless:LoaderIO_VerificationToken"];
             string responseMessage = string.IsNullOrEmpty(loaderIOVerificationToken)
                 ? "This HTTP triggered function executed successfully, however its missing the verification token."
                 : loaderIOVerificationToken;
